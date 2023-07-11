e954820d02ebb5f23446198f3007fe03d376ae7d
Francesco <33670163+Fenerator@users.noreply.github.com>
2023-07-11 22:46:59 +0200

    extraction complete

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # wikiExtract2csv
+
+## Description
+
+This is a simple python script to extract the content of a Wikipedia dump and save it as a csv file compatible with Label Studio.
+
+- samples 100 articles randomly
+- ...
+
+## Usage
+
+1. Download the [Wikipedia dump](https://dumps.wikimedia.org/) corresponding to the desired languge from e.g. [here for ALS](https://dumps.wikimedia.org/alswiki/20230701/), the required file for ALS is [https://dumps.wikimedia.org/alswiki/20230701/alswiki-20230701-pages-articles.xml.bz2](https://dumps.wikimedia.org/alswiki/20230701/alswiki-20230701-pages-articles.xml.bz2).
+2. If not already installed, install Wiki Extractor: `pip install wikiextractor`
+3. Run Wiki Extractor on the dump file, e.g.: `python -m wikiextractor.WikiExtractor alswiki-20230701-pages-articles.xml.bz2`
+4. To convert the data to the required format, run this script on the output of Wiki Extractor: ...
+4. ...
+
+## Dependencies
+
+- wikiextractor
+- lxml
+- ...
---
+++ wikiExtract2csv.py
@@ -0,0 +1,71 @@
+import os, argparse, csv
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+def dir_path(string):    
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+    
+    
+def main(args):
+    # find all files in the input directory
+    input_dir = Path(args.input)
+    output_dir = Path(args.output)
+    print(f'Input directory: {input_dir}')
+    print(f'Output file: {output_dir}')
+    
+    files = list(input_dir.iterdir())
+    
+    # create a new csv file
+    outfile = open(output_dir, 'w')
+    writer = csv.writer(outfile, delimiter =',')
+    writer.writerow(['id','title','url','text'])
+    
+    max_articles = args.max
+
+    counter = 0
+    # for each file, read the contents
+    for file in files:
+
+        with open(file, 'r') as f:
+            data = f.read()
+        
+        soup = BeautifulSoup(data, "lxml")
+        
+    
+        # split into articles
+        articles = soup.find_all('doc')
+        
+        for article in articles:
+            doc_id = article['id']
+            title = article['title']
+            url = article['url']
+            text = article.get_text().strip()
+            
+            assert len(text) > 0 and len(title) > 0 and len(url) > 0 and len(doc_id) > 0, f'Error in {file}: empty field in article {doc_id}, {url}'
+            
+            writer.writerow([doc_id, title, url, text])
+            counter += 1
+            print(f'Processed Article {counter} / {max_articles}')
+            
+            if counter == max_articles:
+                outfile.close()
+                exit()
+            
+    outfile.close()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--input', type=dir_path, required=True, help='file path to the wikiextractor output files, e.g. ../../Documents/alswiki/text/AA')
+    parser.add_argument('--output', type=str, default='articles.csv', help='file path to the output csv file, e.g. ../../Documents/alswiki/text/articles.csv')
+    
+    parser.add_argument('--max', type=int, default=100, help='first n articles to extract (default: 100)')
+    
+    args = parser.parse_args()
+
+    main(args)
